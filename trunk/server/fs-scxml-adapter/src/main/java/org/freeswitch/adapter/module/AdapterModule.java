0cb607d2f2a6d5b793c6c025e1efaf386d7c369d
jocke29@gmail.com <jocke29@gmail.com@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-02-01 18:13:06 +0000

    made it a bundle module in manifest

--- trunk/server/fs-scxml-adapter/src/main/java/org/freeswitch/adapter/module/AdapterModule.java
+++ trunk/server/fs-scxml-adapter/src/main/java/org/freeswitch/adapter/module/AdapterModule.java
@@ -1,6 +1,9 @@
-package org.freeswitch.adapter;
+package org.freeswitch.adapter.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import org.freeswitch.adapter.SessionFactory;
+import org.freeswitch.adapter.SessionFactoryImpl;
 import static org.ops4j.peaberry.Peaberry.service;
 import static org.ops4j.peaberry.util.TypeLiterals.export;
 
@@ -14,6 +17,8 @@ public class AdapterModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(export(SessionFactory.class)).toProvider(service(SessionFactoryImpl.class).export());
+        bind(export(BundleNotifier.class)).toProvider(service(BundleNotifier.class).export());
+        bind(BundleNotifier.class).in(Singleton.class);
     }
 
 }
