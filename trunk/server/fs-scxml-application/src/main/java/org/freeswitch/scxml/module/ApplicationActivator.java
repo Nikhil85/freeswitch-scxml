1f8146f45909b439ffdd49f8ac6d9d7df17098a0
jocke29@gmail.com <jocke29@gmail.com@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-02-25 22:25:38 +0000

    Lookup migration done

--- trunk/server/fs-scxml-application/src/main/java/org/freeswitch/scxml/module/ApplicationActivator.java
+++ trunk/server/fs-scxml-application/src/main/java/org/freeswitch/scxml/module/ApplicationActivator.java
@@ -2,12 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.freeswitch.scxml.module;
 
 import org.apache.commons.scxml.model.CustomAction;
 import org.apache.commons.scxml.model.Var;
-import org.freeswitch.adapter.api.SessionFactory;
 import org.freeswitch.scxml.ApplicationLauncher;
 import org.freeswitch.scxml.ThreadPoolManager;
 import org.freeswitch.scxml.actions.AnswerAction;
@@ -23,6 +21,8 @@ import org.freeswitch.scxml.actions.RecordAudioAction;
 import org.freeswitch.scxml.actions.SayAction;
 import org.freeswitch.scxml.actions.SendAction;
 import org.freeswitch.scxml.actions.WaitAction;
+import org.freeswitch.scxml.engine.ScxmlApplication;
+import org.freeswitch.scxml.engine.ScxmlApplicationImp;
 import org.freeswitch.scxml.engine.ScxmlApplicationLauncher;
 import org.freeswitch.scxml.pool.ThreadPoolManagerImpl;
 import org.freeswitch.scxml.sender.BasicHttpSender;
@@ -30,11 +30,8 @@ import org.freeswitch.scxml.sender.Sender;
 import org.freeswitch.scxml.sender.SenderFactory;
 import org.freeswitch.scxml.sender.SenderFactoryImpl;
 import org.freeswitch.scxml.sender.SipReferSender;
-import org.openide.util.Lookup;
-import org.openide.util.Lookup.Item;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-
 
 /**
  *
@@ -42,17 +39,15 @@ import org.osgi.framework.BundleContext;
  */
 public class ApplicationActivator implements BundleActivator {
 
-     private static final String NAME_SPACE = "http://www.freeswitch.org/";
-    
+    private static final String NAME_SPACE = "http://www.freeswitch.org";
+
     @Override
     public void start(BundleContext context) throws Exception {
-        SessionFactory lookup = Lookup.getDefault().lookup(SessionFactory.class);
-        System.out.println(lookup);
-        
         context.registerService(ApplicationLauncher.class.getName(), new ScxmlApplicationLauncher(), null);
+        context.registerService(ScxmlApplication.class.getName(), new ScxmlApplicationImp(), null);
         context.registerService(ThreadPoolManager.class.getName(), new ThreadPoolManagerImpl(), null);
-        context.registerService(SenderFactory.class.getName(), new SenderFactoryImpl(), null); 
-        context.registerService(Sender.class.getName(), new BasicHttpSender(), null); 
+        context.registerService(SenderFactory.class.getName(), new SenderFactoryImpl(), null);
+        context.registerService(Sender.class.getName(), new BasicHttpSender(), null);
         context.registerService(Sender.class.getName(), new SipReferSender(), null);
         context.registerService(CustomAction.class.getName(), new CustomAction(NAME_SPACE, "menu", MenuAction.class), null);
         context.registerService(CustomAction.class.getName(), new CustomAction(NAME_SPACE, "var", Var.class), null);
@@ -74,5 +69,4 @@ public class ApplicationActivator implements BundleActivator {
     public void stop(BundleContext context) throws Exception {
         //TODO add deactivation code here
     }
-
 }
