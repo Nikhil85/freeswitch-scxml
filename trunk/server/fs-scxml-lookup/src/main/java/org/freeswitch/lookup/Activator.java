7805ad83ef3940e85b2a96c812b9829df6820124
jocke29@gmail.com <jocke29@gmail.com@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-03-06 10:00:10 +0000

    Removed system out statements

--- trunk/server/fs-scxml-lookup/src/main/java/org/freeswitch/lookup/Activator.java
+++ trunk/server/fs-scxml-lookup/src/main/java/org/freeswitch/lookup/Activator.java
@@ -6,24 +6,29 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
    
-    
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+            
     @Override
     public void start(BundleContext context) throws Exception {
+        
         Thread.currentThread().setContextClassLoader(new BundleWrapperClassLoader(context.getBundle()));
         System.setProperty("org.openide.util.Lookup", OsgiLookup.class.getName());
+        
         Lookup lookup = Lookup.getDefault();
 
         if (lookup instanceof OsgiLookup) {
             OsgiLookup ol = (OsgiLookup) lookup;
             context.addServiceListener(ol);
-            System.out.println("System will use lookup " + lookup.getClass().getName());
+            LOG.info("System will use lookup " + lookup.getClass().getName());
             Bundle[] bundles = context.getBundles();
             
             for (int i = 0; i < bundles.length; i++) {
-                registerBundelServices(bundles[i], ol);
+                registerBundleServices(bundles[i], ol);
             }
 
         } else {
@@ -34,7 +39,7 @@ public class Activator implements BundleActivator {
 
     }
 
-    private void registerBundelServices(Bundle bundle, OsgiLookup ol) {
+    private void registerBundleServices(Bundle bundle, OsgiLookup ol) {
         
         ServiceReference[] registeredServices = bundle.getRegisteredServices();
 
