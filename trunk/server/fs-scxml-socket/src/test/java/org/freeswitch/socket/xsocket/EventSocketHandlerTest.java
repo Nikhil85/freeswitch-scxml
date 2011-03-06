82cd477447636fc99171f4fb28afbdfdcd9b3970
jocke29@gmail.com <jocke29@gmail.com@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-03-06 13:23:13 +0000

    Some refactoring

--- trunk/server/fs-scxml-socket/src/test/java/org/freeswitch/socket/xsocket/EventSocketHandlerTest.java
+++ trunk/server/fs-scxml-socket/src/test/java/org/freeswitch/socket/xsocket/EventSocketHandlerTest.java
@@ -2,6 +2,7 @@ package org.freeswitch.socket.xsocket;
 
 import java.io.IOException;
 import org.easymock.EasyMock;
+import org.freeswitch.socket.ServerSessionListener;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,14 +15,16 @@ import org.xsocket.connection.INonBlockingConnection;
  * @author jocke
  */
 public class EventSocketHandlerTest {
-    
+
     private INonBlockingConnection connection;
     private EventSocketHandler handler;
-    
+    private ServerSessionListener serverListener;
+
     @Before
     public void setUp() {
         connection = EasyMock.createMock(INonBlockingConnection.class);
         handler = new EventSocketHandler();
+        serverListener = EasyMock.createMock(ServerSessionListener.class);
     }
 
     @After
@@ -34,72 +37,62 @@ public class EventSocketHandlerTest {
     @Test
     public void testOnDataCommandReply() throws IOException {
         System.out.println("onData command reply");
-        
+
         EasyMock.expect(connection.available()).andReturn(COMMAND_REPLY.length());
         EasyMock.expect(connection.readStringByDelimiter(LINE_BREAKS)).andReturn(COMMAND_REPLY);
-        
-        EasyMock.replay(connection);        
-        assertTrue("Command reply should be succefully handled" , handler.onData(connection));
+
+        EasyMock.replay(connection);
+        assertTrue("Command reply should be succefully handled", handler.onData(connection));
         EasyMock.verify(connection);
     }
-    
+
     /**
      * Test of onData method, of class EventSocketHandler.
      */
     @Test
     public void testOnDataDisconnectNotice() throws IOException {
         System.out.println("onData disconnect notice");
-        
+
         EasyMock.expect(connection.available()).andReturn(DISCONNECT_NOTICE.length());
         EasyMock.expect(connection.readStringByDelimiter(LINE_BREAKS)).andReturn(DISCONNECT_NOTICE);
-        
-        EasyMock.replay(connection);        
-        assertTrue("disconnect notice should be succefully handled" , handler.onData(connection));
+
+        EasyMock.replay(connection);
+        assertTrue("disconnect notice should be succefully handled", handler.onData(connection));
         EasyMock.verify(connection);
     }
+
     /**
      * Test of onData method, of class EventSocketHandler.
      */
     @Test
     public void testOnDataContentLength() throws IOException {
         System.out.println("onData content length");
-        
-        EasyMock.expect(connection.available()).andReturn(DISCONNECT_NOTICE.length());
-        EasyMock.expect(connection.readStringByDelimiter(LINE_BREAKS)).andReturn(DISCONNECT_NOTICE);
-        
-        EasyMock.replay(connection);        
-        assertTrue("disconnect notice should be succefully handled" , handler.onData(connection));
-        EasyMock.verify(connection);
+
+        String body = createBody();
+        String header = createHeader(body);
+
+        EasyMock.expect(connection.available()).andReturn(body.length());
+        EasyMock.expect(connection.readStringByDelimiter(LINE_BREAKS)).andReturn(header);
+        EasyMock.expect(connection.readStringByLength(body.length(), UTF8)).andReturn(body);
+        EasyMock.expect(connection.getAttachment()).andReturn(serverListener);
+        serverListener.onDataEvent(body);
+
+        EasyMock.replay(connection, serverListener);
+        assertTrue("disconnect notice should be succefully handled", handler.onData(connection));
+        EasyMock.verify(connection, serverListener);
     }
 
-//    /**
-//     * Test of onConnect method, of class EventSocketHandler.
-//     */
-//    @Test
-//    public void testOnConnect() {
-//        System.out.println("onConnect");
-//        INonBlockingConnection connection = null;
-//        EventSocketHandler instance = new EventSocketHandler();
-//        boolean expResult = false;
-//        boolean result = instance.onConnect(connection);
-//        assertEquals(expResult, result);
-//         TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of onDisconnect method, of class EventSocketHandler.
-//     */
-//    @Test
-//    public void testOnDisconnect() {
-//        System.out.println("onDisconnect");
-//        INonBlockingConnection connection = null;
-//        EventSocketHandler instance = new EventSocketHandler();
-//        boolean expResult = false;
-//        boolean result = instance.onDisconnect(connection);
-//        assertEquals(expResult, result);
-//         TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    private String createHeader(String body) {
+        return "Content-Length: " + body.length() + "\nContent-Type: text/event-plain" + LINE_BREAKS;
+    }
 
+    private String createBody() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Event-Name: CHANNEL_UUID\n");
+        builder.append("Core-UUID: 4611b345-99a3-4db4-b6aa-1a7886cb5474\n");
+        builder.append("FreeSWITCH-Hostname: centos53_02005\n");
+        builder.append("FreeSWITCH-IPv4: 192.168.2.5");
+        builder.append(LINE_BREAKS);
+        return builder.toString();
+    }
 }
