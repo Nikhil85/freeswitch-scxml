07f8325747f02a0ae953ced282c27a00e7eff970
jocke29 <jocke29@gmail.com>
2011-03-08 22:40:46 +0000

    Fixed all configuration so it can be updated while running.

--- server/fs-scxml-application/src/main/java/org/freeswitch/scxml/engine/ScxmlApplication.java
+++ server/fs-scxml-application/src/main/java/org/freeswitch/scxml/engine/ScxmlApplication.java
@@ -24,34 +24,6 @@ public interface ScxmlApplication {
      *         If the URL can't be resolved do a appropriate resource.
      *
      */
-    void createAndStartMachine(URL url, Map<String, Object> map)
-            throws MalformedURLException;
+    void createAndStartMachine(URL url, Map<String, Object> map) throws MalformedURLException;
 
-    /**
-     * See if the application currently uses a cache.
-     *
-     * @return true when using cache false otherwise.
-     *
-     */
-    boolean isCache();
-
-    /**
-     *
-     * Set if the application should use cache or not.
-     *
-     * @param use To use cache or not.
-     *
-     */
-    void setCache(boolean use);
-
-    /**
-     * Clear the cache from all StateMachines.
-     *
-     * <p>
-     *  Note:
-     *  This will have a performance impact on the
-     *  application. so use wisely.
-     * </p>
-     */
-    void clearCache();
 }
