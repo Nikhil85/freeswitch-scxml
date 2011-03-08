e4499637cc21320431ff7448c2010e6e2d9f20c6
jocke29 <jocke29@gmail.com>
2011-03-08 22:40:33 +0000

    Fixed all configuration so it can be updated while running.

--- server/fs-scxml-adapter/src/main/java/org/freeswitch/adapter/SessionFactoryImpl.java
+++ server/fs-scxml-adapter/src/main/java/org/freeswitch/adapter/SessionFactoryImpl.java
@@ -1,24 +1,56 @@
 package org.freeswitch.adapter;
 
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 import org.freeswitch.adapter.api.SessionFactory;
 import org.freeswitch.adapter.api.Session;
 import java.util.Map;
+import org.freeswitch.config.spi.ConfigChangeListener;
 
 /**
  *
  * @author joe
  */
-public class SessionFactoryImpl implements SessionFactory {
+public class SessionFactoryImpl implements SessionFactory, ConfigChangeListener {
 
-    //private final String path;
-    
-    public SessionFactoryImpl() {
-        //this.path = path;
+    private String path;
+    private static final String RECORDING_PATH = "recording.path";
+    private static final Set<String> KEYS = new HashSet<String>();
+
+    static {
+        KEYS.add(RECORDING_PATH);
     }
 
     @Override
     public Session create(Map<String, Object> map) {
-        map.put(SessionImpl.REC_PATH, /* path */"");
+        map.put(SessionImpl.REC_PATH, path);
         return new SessionImpl(map);
+    }
+
+    @Override
+    public Set<String> getKeys() {
+        return KEYS;
+    }
+
+    @Override
+    public String getValue(String key) {
+        return path;
+    }
+
+    @Override
+    public void setValue(String key, String value) {
+
+        if (value == null) {
+            throw new NullPointerException("Recording path null please provide a valid value");
+        }
+
+        if (new File(value).exists()) {
+            this.path = value;
+        
+        } else {
+            throw new IllegalArgumentException("The recording path must point to an existing folder -->" + value);
+        }
+
     }
 }
