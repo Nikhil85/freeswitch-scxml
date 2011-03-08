2d51abf8713a070776b7c06ffe00077028e52766
jocke29@gmail.com <jocke29@gmail.com@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-03-08 05:58:43 +0000

    Adding configuration module

--- trunk/server/fs-scxml-adapter/src/main/java/org/freeswitch/adapter/osgi/AdapterActivator.java
+++ trunk/server/fs-scxml-adapter/src/main/java/org/freeswitch/adapter/osgi/AdapterActivator.java
@@ -4,6 +4,8 @@ import org.freeswitch.adapter.SessionFactoryImpl;
 import org.freeswitch.adapter.api.SessionFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -11,9 +13,11 @@ import org.osgi.framework.BundleContext;
  */
 public class AdapterActivator implements BundleActivator {
     
+    private static final Logger LOG = LoggerFactory.getLogger(AdapterActivator.class);
+    
     @Override
     public void start(BundleContext context) throws Exception {
-        System.out.println("Starting bundle " + AdapterActivator.class.getName());
+        LOG.info("Starting bundle " + AdapterActivator.class.getName());
         context.registerService(SessionFactory.class.getName(), new SessionFactoryImpl(), null);
     }
 
