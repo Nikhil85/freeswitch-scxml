8771369a8c576723ed0926576024fe285db30108
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-04-02 15:39:14 +0000

    Fixed integration test for menu

--- fs-scxml-integration-test/src/test/java/org/freeswitch/scxml/test/menu/MenuIT.java
+++ fs-scxml-integration-test/src/test/java/org/freeswitch/scxml/test/menu/MenuIT.java
@@ -9,18 +9,31 @@ import org.freeswitch.adapter.api.Event;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.freeswitch.scxml.test.MockConnection.*;
 
 /**
  *
  * @author jocke
  */
 public class MenuIT {
+    
+    public static final String ANSWER = "answer";
+    public static final String PLAYBACK = "playback";
+    public static final String SPEAK = "speak";
    
     private MockConnection con;
     
     @Before
     public void setUp() throws IOException {
         con = new MockConnection(9696);
+        con.connect();
+        onEntry();
+    }
+
+    private void onEntry() throws IOException {
+        con.fireEvent(Event.CHANNEL_DATA, createDataEvent());
+        con.expectApp(ANSWER).andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(PLAYBACK, "say:enter one or two").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
     }
 
     @After
@@ -29,16 +42,42 @@ public class MenuIT {
     }
     
     @Test
-    public void testMatch() throws IOException {
-        con.connect();
-        con.fireEvent(Event.CHANNEL_DATA, createDataEvent());
-        con.expectApp("answer").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
-        con.expectApp("playback").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+    public void testMatchChoiceOne() throws IOException {
         con.fireEvent(DTMF.ONE);
-        con.expectApp("speak").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
-        con.expectApp("speak").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
-        con.expectApp("hangup").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
-        
+        con.expectApp(SPEAK, "Choice one").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(SPEAK, "Bye").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(HANGUP).andReply(Event.CHANNEL_EXECUTE_COMPLETE);  
+    }
+    
+    @Test
+    public void testMatchChoiceTwo() throws IOException {
+        con.fireEvent(DTMF.TWO);
+        con.expectApp(SPEAK, "Choice two").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(SPEAK, "Bye").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(HANGUP).andReply(Event.CHANNEL_EXECUTE_COMPLETE);  
+    }
+  
+    @Test
+    public void testTermdigit() throws IOException {
+        con.fireEvent(DTMF.POUND);
+        con.expectApp(SPEAK, "Termdigit").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(PLAYBACK, "say:enter one or two").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        testMatchChoiceOne();
+    }
+  
+    @Test
+    public void testNoMatch() throws IOException {
+        con.fireEvent(DTMF.FIVE);
+        con.expectApp(SPEAK, "No match").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(PLAYBACK, "say:enter one or two").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        testMatchChoiceOne();
+    }
+    
+    @Test
+    public void testTimeout() throws IOException {
+        con.expectApp(SPEAK, "Max time").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(PLAYBACK, "say:enter one or two").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        testMatchChoiceOne();
     }
     
     private Map<String, String> createDataEvent() {
