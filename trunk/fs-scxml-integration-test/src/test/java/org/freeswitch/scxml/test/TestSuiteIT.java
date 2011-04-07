cb6c206cd3146061d1f284ba084b54822c879470
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-04-07 19:18:20 +0000

    Integration tests are now running in pax exam :)

---
+++ trunk/fs-scxml-integration-test/src/test/java/org/freeswitch/scxml/test/TestSuiteIT.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.freeswitch.scxml.test;
+
+import org.junit.runner.JUnitCore;
+import junit.framework.Assert;
+import java.net.URL;
+import org.ops4j.pax.exam.Inject;
+import java.awt.Menu;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import org.freeswitch.scxml.test.actions.GetDigitsTest;
+import org.freeswitch.scxml.test.actions.MenuTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.BundleContext;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
+
+/**
+ *
+ * @author jocke
+ */
+@RunWith(JUnit4TestRunner.class)
+public class TestSuiteIT {
+
+    @Inject
+    BundleContext context;
+
+    @Test
+    public void runSuite() throws Exception {
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+        JUnitCore.runClasses(MenuTest.class, GetDigitsTest.class);
+    }
+
+    @Configuration
+    public static Option[] configuration() throws Exception {
+        return options(
+                workingDirectory("target/test-classes"),
+                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
+                mavenBundle().groupId("org.xsocket").artifactId("xSocket").version("2.8.14"),
+                mavenBundle().groupId("org.apache.commons").artifactId("com.springsource.org.apache.commons.collections").version("3.2.1"),
+                mavenBundle().groupId("org.apache.commons").artifactId("com.springsource.org.apache.commons.beanutils").version("1.8.0"),
+                mavenBundle().groupId("org.apache.commons").artifactId("com.springsource.org.apache.commons.digester").version("1.8.1"),
+                mavenBundle().groupId("org.ops4j.pax.logging").artifactId("pax-logging-service").version("1.6.1"),
+                mavenBundle().groupId("org.ops4j.pax.logging").artifactId("pax-logging-api").version("1.6.1"),
+                mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.configadmin").version("1.2.8"),
+                mavenBundle().groupId("org.slf4j").artifactId("slf4j-api").version("1.6.1"),
+                mavenBundle().groupId("org.slf4j").artifactId("slf4j-jcl").version("1.6.1"),
+                wrappedBundle(mavenBundle().groupId("commons-jexl").artifactId("commons-jexl").version("1.1")),
+                wrappedBundle(mavenBundle().groupId("xalan").artifactId("xalan").version("2.7.0")),
+                wrappedBundle(mavenBundle().groupId("xml-apis").artifactId("xml-apis").version("2.0.2")),
+                bundle("http://freeswitch-scxml.googlecode.com/files/commons-scxml-0.9.jar"),
+                bundle("http://freeswitch-scxml.googlecode.com/files/org-openide-util-lookup.jar"),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.adapter.api").versionAsInProject(),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.socket").versionAsInProject(),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.adapter").versionAsInProject(),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.application").versionAsInProject(),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.socket.api").versionAsInProject(),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.application.api").versionAsInProject(),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.lookup").versionAsInProject(),
+                mavenBundle("org.freeswitch.scxml", "org.freeswitch.scxml.config").versionAsInProject(),
+                felix());
+    }
+}
