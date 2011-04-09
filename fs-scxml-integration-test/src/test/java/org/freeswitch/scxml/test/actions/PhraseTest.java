f90c071ae97ce7f213cc990c5007721fecdca728
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-04-09 14:03:50 +0000

    Added phrase action test

---
+++ fs-scxml-integration-test/src/test/java/org/freeswitch/scxml/test/actions/PhraseTest.java
@@ -0,0 +1,43 @@
+package org.freeswitch.scxml.test.actions;
+
+import org.freeswitch.adapter.api.Event;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
+import org.freeswitch.scxml.test.MockConnection;
+import org.junit.After;
+import org.junit.Before;
+import static org.freeswitch.scxml.test.MockConnection.*;
+
+/**
+ *
+ * @author jocke
+ */
+public class PhraseTest {
+
+    private MockConnection con;
+
+    @Before
+    public void setUp() throws IOException {
+        con = new MockConnection(9696);
+        con.connect();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+     con.close();
+    }
+
+    public void testPhrase() throws IOException {
+        con.fireEvent(Event.CHANNEL_DATA, createDataEvent());
+        con.expectApp(ANSWER).andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(SAY, "en number iterated '1234'").andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+        con.expectApp(HANGUP).andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+    }
+
+    private Map<String, String> createDataEvent() {
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("variable_scxml", this.getClass().getClassLoader().getResource("org/freeswitch/scxml/test/phraseTest.xml").toString());
+        return data;
+    }
+}
