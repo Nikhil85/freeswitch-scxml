b0ed4f710587be2828ffe535ea80d4a796d7d859
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-04-17 15:18:31 +0000

    Small fix

---
+++ fs-scxml-integration-test/src/test/java/org/freeswitch/scxml/test/actions/RecordAudioTest.java
@@ -0,0 +1,43 @@
+package org.freeswitch.scxml.test.actions;
+
+import org.junit.Test;
+import org.freeswitch.adapter.api.Event;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
+import org.freeswitch.scxml.test.MockConnection;
+import org.junit.After;
+import org.junit.Before;
+import static org.freeswitch.scxml.test.MockConnection.*;
+
+/**
+ *
+ * @author jocke
+ */
+public class RecordAudioTest {
+
+    private MockConnection con;
+
+    @Before
+    public void setUp() throws IOException {
+        con = new MockConnection(9696);
+        con.connect();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+     con.close();
+    }
+    
+    @Test
+    public void testRecordAudioTermDigit() throws IOException {
+        con.fireEvent(Event.CHANNEL_DATA, createDataEvent());
+        con.expectApp(ANSWER).andReply(Event.CHANNEL_EXECUTE_COMPLETE);
+    }
+
+    private Map<String, String> createDataEvent() {
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("variable_scxml", this.getClass().getClassLoader().getResource("org/freeswitch/scxml/test/recordAudioTest.xml").toString());
+        return data;
+    }
+}
