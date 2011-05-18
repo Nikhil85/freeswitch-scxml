4cf17b91cb5dea75521a1f86e6d59fe8964c33fd
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-05-18 08:51:24 +0000

    Failing test

--- trunk/fs-scxml-adapter/src/test/java/org/freeswitch/adapter/TestExtension.java
+++ trunk/fs-scxml-adapter/src/test/java/org/freeswitch/adapter/TestExtension.java
@@ -2,11 +2,13 @@ package org.freeswitch.adapter;
 
 import org.freeswitch.adapter.api.Extension;
 import org.freeswitch.adapter.api.Session;
+import org.junit.Ignore;
 
 /**
  *
  * @author jocke
  */
+@Ignore
 public class TestExtension implements Extension {
     private Session session;
     
