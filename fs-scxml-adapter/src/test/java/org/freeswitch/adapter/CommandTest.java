72f0e49c85f0098e8ad949625dd2d52352d5dfa6
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-05-22 18:28:44 +0000

    Failing test

---
+++ fs-scxml-adapter/src/test/java/org/freeswitch/adapter/CommandTest.java
@@ -0,0 +1,90 @@
+package org.freeswitch.adapter;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jocke
+ */
+public class CommandTest {
+    
+    public CommandTest() {
+    }
+
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of answer method, of class Command.
+     */
+    @Test
+    public void testAnswer() {
+    }
+
+    /**
+     * Test of hangup method, of class Command.
+     */
+    @Test
+    public void testHangup() {
+    }
+
+    /**
+     * Test of breakcommand method, of class Command.
+     */
+    @Test
+    public void testBreakcommand() {
+    }
+
+    /**
+     * Test of speak method, of class Command.
+     */
+    @Test
+    public void testSpeak() {
+    }
+
+    /**
+     * Test of say method, of class Command.
+     */
+    @Test
+    public void testSay() {
+    }
+
+    /**
+     * Test of record method, of class Command.
+     */
+    @Test
+    public void testRecord() {
+    }
+
+    /**
+     * Test of set method, of class Command.
+     */
+    @Test
+    public void testSet() {
+    }
+
+    /**
+     * Test of refer method, of class Command.
+     */
+    @Test
+    public void testRefer() {
+    }
+
+    /**
+     * Test of playback method, of class Command.
+     */
+    @Test
+    public void testPlayback() {
+    }
+}
