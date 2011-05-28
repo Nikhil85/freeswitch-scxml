636b5d3be7189faafa232c65b0eaa9d98505cbdc
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-05-28 21:06:37 +0000

    refactoring

--- trunk/fs-scxml-application/src/main/java/org/freeswitch/scxml/actions/CallAction.java
+++ trunk/fs-scxml-application/src/main/java/org/freeswitch/scxml/actions/CallAction.java
@@ -1,21 +1,69 @@
 package org.freeswitch.scxml.actions;
 
-import org.freeswitch.adapter.api.CallAdapter;
+import org.freeswitch.adapter.api.event.Event;
+import org.freeswitch.adapter.api.event.EventQueueListener;
 import org.freeswitch.adapter.api.HangupException;
-import org.freeswitch.adapter.api.Session;
+import org.freeswitch.adapter.api.session.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author jocke
  */
 public class CallAction extends AbstractAction {
-    private static final long serialVersionUID = 1L;
-    
+
+    private static final Logger LOG = LoggerFactory.getLogger(CallAction.class);
     private String value;
-    
+    private String url;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     @Override
-    public void handleAction(Session fss) throws HangupException {
-        CallAdapter adapter = fss.lookup(CallAdapter.class);
-        adapter.originate(value);
+    public void handleAction(Session session) throws HangupException {
+
+        LOG.warn("Hello I will make call");
+
+        if (notValid()) {
+            return;
+        }
+
+        session.call(eval(value), new EventListener());
+
+    }
+
+    private boolean notValid() {
+        if (!validateFields(value)) {
+            LOG.error("Invalid action {}", this);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "CallAction{" + "value=" + value + ", url=" + url + '}';
+    }
+
+    private class EventListener implements EventQueueListener {
+
+        @Override
+        public void onAdd(Event event) {
+            System.out.println(event);
+        }
     }
 }
