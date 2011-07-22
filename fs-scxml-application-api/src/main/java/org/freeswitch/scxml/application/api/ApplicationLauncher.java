4405e3a2c293bf347be80e083cd92dddbd13c20e
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-07-22 11:04:09 +0000

    fixed error print from logging framework.

--- fs-scxml-application-api/src/main/java/org/freeswitch/scxml/application/api/ApplicationLauncher.java
+++ fs-scxml-application-api/src/main/java/org/freeswitch/scxml/application/api/ApplicationLauncher.java
@@ -2,32 +2,39 @@ package org.freeswitch.scxml.application.api;
 
 import org.freeswitch.adapter.api.session.Session;
 
-
 /**
- *  This interface is used for launching
- *  new IVR applications.
- *
+ * This interface is used for launching new IVR applications.
+ * 
  * @author jocke
  */
 public interface ApplicationLauncher {
 
     /**
      * Launch a new IVR application.
-     *
-     * <p>
-     * An implementation of this interface should be expected to be
-     * called by multiple threads, hence it it should be thread safe.
-     * </p>
-     *
-     * <p>
-     *  When a call comes in a new IvrSession is created
-     *  along with a dedicated Thread. An implementation should
-     *  start execute the application logic in that thread.
-     * </p>
-     *
-     * @param  session
-     *         A session that could be used to execute Ivr applications.
-     *
+     * <p/>
+     * <p> An implementation of this interface should be expected to be
+     * called by multiple threads, hence it it should be thread safe. </p>
+     * <p/>
+     * <p> When a call comes in a new IvrSession is created along with a
+     * dedicated Thread. An implementation should start execute the
+     * application logic in that thread. </p>
+     * <p/>
+     * @param session A session that could be used to execute Ivr
+     * applications.
+     * <p/>
      */
     void launch(Session session);
+    
+    /**
+     * Test if this session can be launched by this
+     * application launcher. This method should
+     * not hijack the session and run it in a different Thread.
+     * 
+     * @param session 
+     *       The session to launch.
+     * 
+     * @return {@code true} if launchable {@code false } otherwise.
+     * 
+     */
+    boolean isLaunchable(Session session);
 }
