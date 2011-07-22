cb157ac0f6cf77606218230ec028652c171ba738
jocke29 <jocke29@1391ae4f-1b82-c798-1bfe-20a1e7e0af7b>
2011-07-22 11:04:57 +0000

    fixed error print from logging framework.

--- trunk/fs-scxml-socket/src/main/java/org/freeswitch/socket/xsocket/ApplicationRunner.java
+++ trunk/fs-scxml-socket/src/main/java/org/freeswitch/socket/xsocket/ApplicationRunner.java
@@ -1,5 +1,6 @@
 package org.freeswitch.socket.xsocket;
 
+import java.util.Collection;
 import org.freeswitch.adapter.api.session.Session;
 import org.freeswitch.scxml.application.api.ApplicationLauncher;
 import org.openide.util.Lookup;
@@ -7,28 +8,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ApplicationRunner implements Runnable {
-
+    
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationRunner.class);
     private final Session fss;
-
+    
     public ApplicationRunner(Session fss) {
         this.fss = fss;
     }
-
+    
     @Override
     public void run() {
-        ApplicationLauncher launcher = Lookup.getDefault().lookup(ApplicationLauncher.class);
-        
-        if(launcher == null) {
-            LOG.warn("No application launcher found. Unable to launch!!");
-            return;
-        }
-        
         try {
-            launcher.launch(fss);
+   
+            for (ApplicationLauncher launcher : getLaunchers()) {
+                if (launcher.isLaunchable(fss)) {
+                    launcher.launch(fss);
+                    fss.hangup();
+                    return;
+                }
+            }
+            
+            LOG.warn("No launcher found for this session");
         } catch (Exception ex) {
             LOG.error("Application Thread died \n", ex);
         }
-        fss.hangup();
+        
+    }
+    
+    private Collection<? extends ApplicationLauncher> getLaunchers() {
+        return Lookup.getDefault().lookupAll(ApplicationLauncher.class);
     }
 }
